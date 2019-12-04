aa5dbb0ebba7c01c3b42a9784637c84e81f5dc1d
Vincenzo Territo <territo.vincenzo17@gmail.com>
2019-12-04 19:42:22 +0100

    content: small changes + tests for grid

--- src/content.rs
+++ src/content.rs
@@ -36,24 +36,55 @@ impl Grid {
         }
     }
 
-    fn update_cell(&mut self, r: usize, c: usize, p: CellContent) -> Result<(), &str> {
+    fn update_cell(&mut self, r: usize, c: usize, ph: CellContent) -> Result<(), &str> {
         if (r > 2) || (c > 2) {
             return Err("row or column index out of bound");
         }
         if self.content[r][c] != CellContent::Nil {
             return Err("non empty cell");
         }
-        self.content[r][c] = p;
+        self.content[r][c] = ph;
         Ok(())
     }
 
-    pub fn player1_move(&mut self, r: usize, c: usize) -> Result<(), &str> {
-        self.update_cell(r, c, CellContent::Ph1)?;
+    pub fn player_move(&mut self, r: usize, c: usize, player_id: u32) -> Result<(), &str> {
+        if player_id > 1 {
+            panic!("Invalid player id!");
+        }
+
+        let place_holder = if player_id == 0 {
+            CellContent::Ph1
+        } else {
+            CellContent::Ph2
+        };
+
+        self.update_cell(r, c, place_holder)?;
         Ok(())
     }
+}
 
-    pub fn player2_move(&mut self, r: usize, c: usize) -> Result<(), &str> {
-        self.update_cell(r, c, CellContent::Ph2)?;
-        Ok(())
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn wrong_player_move() {
+        let mut g = Grid::new();
+        g.player_move(1, 2, 5);
+    }
+
+    #[test]
+    #[should_panic]
+    fn wrong_cell_content() {
+        let mut g = Grid::new();
+        g.cell_content(1, 8);
+    }
+
+    #[test]
+    fn grid_test() {
+        let mut g = Grid::new();
+        g.player_move(1, 2, 0);
+        assert_eq!('X', g.cell_content(1, 2));
     }
 }
